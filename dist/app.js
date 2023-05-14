a59fd73354624935cab7287c80fb314085274fef
Esteban <esteban@fwd.dev>
2023-05-14 18:53:48 -0400

    fix

--- dist/app.js
+++ dist/app.js
@@ -283,6 +283,14 @@ var nano = new Vue({
       }
     },
     methods: {
+      plan_title(string) {
+        console.log(string)
+        if (string && string.includes('POW Credits')) {
+          string = `${this.kFormatter(string.replace('POW Credits', '').split(',').join(''))} POW Credits`
+          // string = `${this.nFormatter(string.replace('POW Credits', '').split(',').join(''))} POW Credits`
+        }
+        return string
+      },
       whois(prompt) {
         var string = `Verified as @${prompt.github} on Github.` 
         if (
@@ -301,6 +309,22 @@ var nano = new Vue({
           return window.alert(`Verified as @${prompt.github} on Github and Twitter.`)
         }
         return window.alert(string)
+      },
+      kFormatter(num, digits) {
+        const lookup = [
+          { value: 1, symbol: "" },
+          { value: 1e3, symbol: "K" },
+          { value: 1e6, symbol: "M" },
+          { value: 1e9, symbol: "G" },
+          { value: 1e12, symbol: "T" },
+          { value: 1e15, symbol: "P" },
+          { value: 1e18, symbol: "E" }
+        ];
+        const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+        var item = lookup.slice().reverse().find(function(item) {
+          return num >= item.value;
+        });
+        return item ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol : "0";
       },
       nFormatter(num) {
         return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
