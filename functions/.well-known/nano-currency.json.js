8c8d3cf1fb13a0b7f9b2a49f20574451adc524e1
Esteban <esteban@fwd.dev>
2023-05-14 17:41:09 -0400

    latest

--- functions/.well-known/nano-currency.json.js
+++ functions/.well-known/nano-currency.json.js
@@ -1,3 +1,17 @@
+// Respond to OPTIONS method
+export async function onRequestOptions(next) {
+// export const onRequestOptions: PagesFunction = async () => {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
+      'Access-Control-Allow-Methods': 'GET, OPTIONS',
+      'Access-Control-Max-Age': '86400',
+    },
+  });
+};
+
 export async function onRequestGet(ctx) {
 
   let url = new URL(ctx.request.url);
