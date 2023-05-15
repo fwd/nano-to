09d03ebf8e8defecf6e2ca3ab419528a2448e651
Esteban <esteban@fwd.dev>
2023-05-14 20:01:24 -0400

    cloudflare functions dammit

--- functions/.well-known/nano-currency.json.js
+++ functions/.well-known/nano-currency.json.js
@@ -1,24 +1,3 @@
-// export async function onRequest(next) {
-//   const response = await next();
-//   response.headers.set('Access-Control-Allow-Origin', '*');
-//   response.headers.set('Access-Control-Max-Age', '86400');
-//   return response;
-// };
-
-// // Respond to OPTIONS method
-// export async function onRequestOptions(next) {
-// // export const onRequestOptions: PagesFunction = async () => {
-//   return new Response(null, {
-//     status: 204,
-//     headers: {
-//       'Access-Control-Allow-Origin': '*',
-//       'Access-Control-Allow-Headers': '*',
-//       'Access-Control-Allow-Methods': 'GET, OPTIONS',
-//       'Access-Control-Max-Age': '86400',
-//     },
-//   });
-// };
-
 export async function onRequestGet(ctx) {
 
   let url = new URL(ctx.request.url);
@@ -41,22 +20,10 @@ export async function onRequestGet(ctx) {
   const _response = await fetch('https://nano.to/known.json', init);
   const results = await gatherResponse(_response);
   let name = url.searchParams.get('names');
-      name = name ? name : ''
-
-  // return new Response(, {
-  //   status: 200,
-  //   headers: {
-  //     "content-type": "application/json;charset=UTF-8",
-  //     // 'Set-Cookie': `cf_clearance=invalid; expires=Thu, 01 Jan 1970 00:00:00 GMT; Max-Age=0; path=/; domain=.${domain}`,
-  //   },
-  // });
+      name = name ? name.replace('@', '') : ''
 
   const response = Response.json({ names: JSON.parse(results).filter(a => a.name.toLowerCase() === name.toLowerCase()) });
   response.headers.set('Access-Control-Allow-Origin', '*');
   return response;
-
-  // return new Response({ names: results }, {
-  // return Response.json({ names: JSON.parse(results)[0] });
-  // return Response.json({ names: JSON.parse(results).filter(a => a.name.toLowerCase() === name.toLowerCase()) });
 
 }
