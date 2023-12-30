b6576a6baa2a1d48ae52dee294f3611b9d55ca54
Esteban <esteban@fwd.dev>
2023-12-30 15:08:21 -0500

    9.9.0

--- dist/app.js
+++ dist/app.js
@@ -1241,7 +1241,7 @@ var nano = new Vue({
             }
             if (!suggestion.website_button_only) {
                 _prompt.body = `
-<p style="margin-top:0" onclick="window.copy('${suggestion.address}')" style="font-size: 28px;text-transform: lowercase;word-break: break-word;max-width: 420px;font-family: 'Cyber';text-align: center;width: 100%;display: inline-block;margin-top: 0px;margin-bottom: 0px;text-shadow: rgb(49 49 49 / 0%) 2px 2px 0px;letter-spacing: 3px;">
+<p onclick="window.copy('${suggestion.address}')" style="font-size: 28px;text-transform: lowercase;word-break: break-word;max-width: 420px;font-family: 'Cyber';text-align: center;width: 100%;display: inline-block;margin-top: 0px;margin-bottom: 0px;text-shadow: rgb(49 49 49 / 0%) 2px 2px 0px;letter-spacing: 3px;">
 <span style="color: rgb(255 56 62);">${suggestion.address.slice(0, 12)}</span>${suggestion.address.slice(12, 59)}<span style="color: rgb(255 56 62);">${suggestion.address.slice(59, 99)}</span>
 </p>`
             }
